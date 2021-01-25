da61319071c98f62e733824c91d72be30d0d1386
Raphael Barreto <tnt.raphael@gmail.com>
2021-01-25 10:08:45 -0300

    Pages & Routes

---
+++ pages/about.js
@@ -0,0 +1,15 @@
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam est eos
+        officia illum, debitis quas mollitia! Assumenda aspernatur sequi,
+        obcaecati voluptate temporibus quis dolore suscipit quas explicabo
+        incidunt! Sunt, quae.
+      </p>
+    </div>
+  );
+};
+
+export default About;
