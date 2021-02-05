8d16fbc7767c6c56ea49940d8ba26c4cf6170451
Raphael Barreto <tnt.raphael@gmail.com>
2021-02-04 21:37:02 -0300

    Adding Styles

--- pages/index.js
+++ pages/index.js
@@ -7,15 +7,17 @@ import styles from '../styles/Home.module.css';
 export default function Home() {
   return (
     <div>
-      <h1>Homepage</h1>
-      <p>
+      <h1 className={styles.title}>Homepage</h1>
+      <p className={styles.text}>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur
         quaerat nemo excepturi voluptatibus voluptatem laudantium cumque soluta
         molestias quibusdam suscipit in, nulla ratione! Animi odit voluptate
         impedit rerum numquam? Tempora?
       </p>
       <Link href="/ninjas">
-        <a>See Ninja Listing</a>
+        <a className={styles.btn} href>
+          See Ninja Listing
+        </a>
       </Link>
     </div>
   );
