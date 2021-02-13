f736c71b124ef21a746e75655d4d90d6d15bf682
Raphael Barreto <tnt.raphael@gmail.com>
2021-02-13 19:23:55 -0300

    Fetching a Single Item

--- pages/ninjas/[id].js
+++ pages/ninjas/[id].js
@@ -1,7 +1,7 @@
 export const getStaticPaths = async () => {
   const response = await fetch('https://jsonplaceholder.typicode.com/users');
 
-  const data = await response.json;
+  const data = await response.json();
 
   const paths = data.map((ninja) => {
     return {
@@ -10,15 +10,32 @@ export const getStaticPaths = async () => {
   });
 
   return {
-    paths: paths,
+    paths,
     fallback: false,
   };
 };
 
-const Details = () => {
+export const getStaticProps = async (context) => {
+  const id = context.params.id;
+
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/users/${id}`
+  );
+
+  const data = await response.json();
+
+  return {
+    props: { ninja: data },
+  };
+};
+
+const Details = ({ ninja }) => {
   return (
     <div>
-      <h1>Details Page</h1>
+      <h1>{ninja.name}</h1>
+      <p>{ninja.email}</p>
+      <p>{ninja.website}</p>
+      <p>{ninja.address.city}</p>
     </div>
   );
 };
