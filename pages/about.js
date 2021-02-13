e69b638c78df55ce8e3c64e6b73b371cbfdc5f4f
Raphael Barreto <tnt.raphael@gmail.com>
2021-02-13 18:35:19 -0300

    Images & Metadata

--- pages/about.js
+++ pages/about.js
@@ -1,14 +1,22 @@
+import Head from 'next/head';
+
 const About = () => {
   return (
-    <div>
-      <h1>About</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam est eos
-        officia illum, debitis quas mollitia! Assumenda aspernatur sequi,
-        obcaecati voluptate temporibus quis dolore suscipit quas explicabo
-        incidunt! Sunt, quae.
-      </p>
-    </div>
+    <>
+      <Head>
+        <title>Ninja List | About</title>
+        <meta name="keywords" content="ninjas" />
+      </Head>
+      <div>
+        <h1>About</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam est
+          eos officia illum, debitis quas mollitia! Assumenda aspernatur sequi,
+          obcaecati voluptate temporibus quis dolore suscipit quas explicabo
+          incidunt! Sunt, quae.
+        </p>
+      </div>
+    </>
   );
 };
 
